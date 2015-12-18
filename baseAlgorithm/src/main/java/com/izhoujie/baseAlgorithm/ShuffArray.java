0cf9767f7ae574428ed501c4c8f4e273dc3fed73
ZhouJie <macula@vip.qq.com>
2015-12-18 19:35:18 +0800

    数组Util-Tools

---
+++ baseAlgorithm/src/main/java/com/izhoujie/baseAlgorithm/ShuffArray.java
@@ -0,0 +1,30 @@
+package com.izhoujie.baseAlgorithm;
+
+import java.util.Random;
+
+public class ShuffArray {
+	public static void main(String[] args) {
+
+	}
+
+	/**
+	 * @param RangeScope
+	 *            数据值范围
+	 * @param length
+	 *            数组长度
+	 * @return
+	 */
+	public static int[] getArray(int RangeScope, int length) {
+
+		Random random = new Random();
+		int[] array = new int[length];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = random.nextInt(RangeScope);
+		}
+
+		return array;
+	}
+
+	public static void printArray(int[] array) {
+	}
+}
