e4b69da97080a8ebd177d3de936fa3ade6726036
ZhouJie <macula@vip.qq.com>
2015-12-21 14:50:05 +0800

    折半插入排序
    Signed-off-by: ZhouJie <admin@izhoujie.com>

---
+++ baseAlgorithm/src/main/java/com/izhoujie/baseAlgorithm/BinarySelectSort.java
@@ -0,0 +1,42 @@
+package com.izhoujie.baseAlgorithm;
+
+import java.util.Arrays;
+
+/**
+ * @author km-zhou
+ *
+ *         -折半插入排序
+ *
+ *         -时间复杂度：O(n*n)
+ *
+ *         -稳定性：稳定
+ */
+public class BinarySelectSort {
+	public static void main(String[] args) {
+		int range = 100;
+		int length = 25;
+
+		int[] array = ShuffArray.getArray(range, length);
+		System.out.println("排序前：" + Arrays.toString(array));
+		selectSort(array);
+		System.out.println("排序后：" + Arrays.toString(array));
+	}
+
+	public static void selectSort(int[] array) {
+		int len = array.length;
+		int temp;
+		for (int i = 1; i < len; i++) {
+
+			if (array[i] < array[i - 1]) {
+				
+				for (int j = i; j > 0; j--) {
+					if (array[j] < array[j - 1]) {
+						temp = array[j];
+						array[j] = array[j - 1];
+						array[j - 1] = temp;
+					}
+				}
+			}
+		}
+	}
+}
