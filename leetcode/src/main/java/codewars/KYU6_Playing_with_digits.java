12cf32c7ef5414fddcd274b9a3a21de6da84ab11
ZhouJie <macula@vip.qq.com>
2020-02-27 00:08:10 +0800

    - Playing with digits

---
+++ leetcode/src/main/java/codewars/KYU6_Playing_with_digits.java
@@ -0,0 +1,53 @@
+package codewars;
+
+/**
+ * @author ZhouJie
+ * @date 2020年2月26日 下午10:15:46 
+ * @Description: Playing with digits
+ *
+	Some numbers have funny properties. For example:
+	
+	89 --> 8¹ + 9² = 89 * 1
+	
+	695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
+	
+	46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+	
+	Given a positive integer n written as abcd... (a, b, c, d... being digits) and a positive integer p
+	
+	we want to find a positive integer k, if it exists, such as the sum of the digits of n taken to the successive powers of p is equal to k * n.
+	In other words:
+	
+	Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
+	
+	If it is the case we will return k, if not return -1.
+	
+	Note: n and p will always be given as strictly positive integers.
+	
+	digPow(89, 1) should return 1 since 8¹ + 9² = 89 = 89 * 1
+	digPow(92, 1) should return -1 since there is no k such as 9¹ + 2² equals 92 * k
+	digPow(695, 2) should return 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
+	digPow(46288, 3) should return 51 since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+ */
+public class KYU6_Playing_with_digits {
+	/**
+	 * @author: ZhouJie
+	 * @date: 2020年2月26日 下午10:16:09 
+	 * @param: @param n
+	 * @param: @param p
+	 * @param: @return
+	 * @return: long
+	 * @Description: 1-逐位幂和再用和对p取模校验；
+	 *
+	 */
+	public static long digPow(int n, int p) {
+		int len = String.valueOf(n).length();
+		p += len - 1;
+		long rst = 0, m = n;
+		while (n > 0) {
+			rst += Math.pow(n % 10, p--);
+			n /= 10;
+		}
+		return rst % m == 0 ? rst / m : -1;
+	}
+}
