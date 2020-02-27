a3031736e3d8204f3a2a453971e3141c296ce490
ZhouJie <macula@vip.qq.com>
2020-02-27 19:27:43 +0800

    - Sum of Digits / Digital Root

---
+++ leetcode/src/main/java/codewars/KYU6_Sum_of_Digits_Or_Digital_Root.java
@@ -0,0 +1,65 @@
+package codewars;
+
+/**
+ * @author ZhouJie
+ * @date 2020年2月27日 下午6:48:33 
+ * @Description: Sum of Digits / Digital Root
+ *
+	In this kata, you must create a digital root function.
+	
+	A digital root is the recursive sum of all the digits in a number. Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. This is only applicable to the natural numbers.
+	
+	Here's how it works:
+	
+	digital_root(16)
+	=> 1 + 6
+	=> 7
+	
+	digital_root(942)
+	=> 9 + 4 + 2
+	=> 15 ...
+	=> 1 + 5
+	=> 6
+	
+	digital_root(132189)
+	=> 1 + 3 + 2 + 1 + 8 + 9
+	=> 24 ...
+	=> 2 + 4
+	=> 6
+	
+	digital_root(493193)
+	=> 4 + 9 + 3 + 1 + 9 + 3
+	=> 29 ...
+	=> 2 + 9
+	=> 11 ...
+	=> 1 + 1
+	=> 2
+ */
+public class KYU6_Sum_of_Digits_Or_Digital_Root {
+
+	/**
+	 * @author: ZhouJie
+	 * @date: 2020年2月27日 下午7:20:26 
+	 * @param: @param n
+	 * @param: @return
+	 * @return: int
+	 * @Description: 1-
+	 *
+	 */
+	public static int digital_root_1(int n) {
+		return n < 9 ? n : digital_root_1(n / 10 + n % 10);
+	}
+
+	/**
+	 * @author: ZhouJie
+	 * @date: 2020年2月27日 下午7:20:59 
+	 * @param: @param n
+	 * @param: @return
+	 * @return: int
+	 * @Description: TODO
+	 *
+	 */
+	public static int digital_root_2(int n) {
+		return (n != 0 && n % 9 == 0) ? 9 : n % 9;
+	}
+}
