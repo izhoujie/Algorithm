e841550142ec0c8314f0356c8977ec4e61bd1e3c
ZhouJie <macula@vip.qq.com>
2021-07-06 16:18:37 +0800

    189. 旋转数组

---
+++ leetcode/src/main/java/leetcode/LeetCode_0189.java
@@ -0,0 +1,76 @@
+package leetcode;
+
+/**
+ * @author zhoujie
+ * @date 2021/7/6 下午3:46
+ * @description: 189. 旋转数组
+ * <p>
+ * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+ * <p>
+ *  
+ * <p>
+ * 进阶：
+ * <p>
+ * 尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+ * 你可以使用空间复杂度为 O(1) 的 原地 算法解决这个问题吗？
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums = [1,2,3,4,5,6,7], k = 3
+ * 输出: [5,6,7,1,2,3,4]
+ * 解释:
+ * 向右旋转 1 步: [7,1,2,3,4,5,6]
+ * 向右旋转 2 步: [6,7,1,2,3,4,5]
+ * 向右旋转 3 步: [5,6,7,1,2,3,4]
+ * 示例 2:
+ * <p>
+ * 输入：nums = [-1,-100,3,99], k = 2
+ * 输出：[3,99,-1,-100]
+ * 解释:
+ * 向右旋转 1 步: [99,-1,-100,3]
+ * 向右旋转 2 步: [3,99,-1,-100]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 2 * 104
+ * -231 <= nums[i] <= 231 - 1
+ * 0 <= k <= 105
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/rotate-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode_0189 {
+}
+
+class Solution_0189 {
+    /**
+     * @return void
+     * @author zhoujie
+     * @date 2021/7/6 下午3:58
+     * @param: nums
+     * @param: k
+     * @description: 翻转，先整体翻转，再以k为分割左右分别翻转
+     */
+    public void rotate(int[] nums, int k) {
+        int len = nums.length;
+        k %= len;
+        reverse(nums, 0, len - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, len - 1);
+
+    }
+
+    private void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
