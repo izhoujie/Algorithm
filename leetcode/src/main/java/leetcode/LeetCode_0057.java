3fafebf13772be50d239e3cdd3b4fd657700f947
ZhouJie <macula@vip.qq.com>
2023-06-04 14:30:53 +0800

    57. 插入区间 注释格式调整

--- leetcode/src/main/java/leetcode/LeetCode_0057.java
+++ leetcode/src/main/java/leetcode/LeetCode_0057.java
@@ -7,31 +7,44 @@ import java.util.List;
  * @author zhoujie
  * @date 2020/11/4 09:26
  * @description: 57. 插入区间
- * <p>
- * 给出一个无重叠的 ，按照区间起始端点排序的区间列表。
- * <p>
+ *
+ * 给你一个 无重叠的 ，按照区间起始端点排序的区间列表。
+ *
  * 在列表中插入一个新的区间，你需要确保列表中的区间仍然有序且不重叠（如果有必要的话，可以合并区间）。
- * <p>
- *  
- * <p>
- * 示例 1：
- * <p>
+ *
+ *
+ *
+ * 示例 1：
+ *
  * 输入：intervals = [[1,3],[6,9]], newInterval = [2,5]
  * 输出：[[1,5],[6,9]]
- * 示例 2：
- * <p>
+ * 示例 2：
+ *
  * 输入：intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
  * 输出：[[1,2],[3,10],[12,16]]
- * 解释：这是因为新的区间 [4,8] 与 [3,5],[6,7],[8,10] 重叠。
- *  
- * <p>
- * 注意：输入类型已在 2019 年 4 月 15 日更改。请重置为默认代码定义以获取新的方法签名。
- * <p>
- * <p>
- * <p>
- * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/insert-interval
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 解释：这是因为新的区间 [4,8] 与 [3,5],[6,7],[8,10] 重叠。
+ * 示例 3：
+ *
+ * 输入：intervals = [], newInterval = [5,7]
+ * 输出：[[5,7]]
+ * 示例 4：
+ *
+ * 输入：intervals = [[1,5]], newInterval = [2,3]
+ * 输出：[[1,5]]
+ * 示例 5：
+ *
+ * 输入：intervals = [[1,5]], newInterval = [2,7]
+ * 输出：[[1,7]]
+ *
+ *
+ * 提示：
+ *
+ * 0 <= intervals.length <= 104
+ * intervals[i].length == 2
+ * 0 <= intervals[i][0] <= intervals[i][1] <= 105
+ * intervals 根据 intervals[i][0] 按 升序 排列
+ * newInterval.length == 2
+ * 0 <= newInterval[0] <= newInterval[1] <= 105
  */
 public class LeetCode_0057 {
 }
